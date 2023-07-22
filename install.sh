7393b4e49f3b72af89628a6e55a574c733acff4a
osarthak <hi99@tutanota.com>
2023-07-22 19:56:06 +0000

    rm firefox features

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 pkill firefox
 rm -rf $HOME/.mozilla/firefox/*.default-release/*
 cp firefox/* $HOME/.mozilla/firefox/*.default-release
+echo "removing /usr/lib64/firefox/browser/features/*"
+sudo rm -I /usr/lib64/firefox/browser/features/*
 
 ublock_version="1.46.0"
 wget -O uBlock0_$ublock_version.firefox.signed.xpi https://github.com/gorhill/uBlock/releases/download/$ublock_version/uBlock0_$ublock_version.firefox.signed.xpi
